9513585ab4ca0dadc7214ab7bc48fa4ed714d33e
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-03-21 14:46:26 +0300

    Добавьте файлы проекта.

---
+++ LabWpfApp19/Models/Length.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWpfApp19.Models
+{
+    static class Length
+    { 
+       public static double Add(double a, double b) =>a * b;
+    }
+}
---
+++ LabWpfApp19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using LabWpfApp19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LabWpfApp19.ViewModels
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double number1;
+        public double Number1
+        {
+            get => number1;
+            set
+            {
+                number1 = 2*Math.PI;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+
+            }
+        }
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecuted(object p)
+        {
+            Number3 = Length.Add( Number1, Number2);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1 != 0 || Number2 !=0)
+            return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand (OnAddCommandExecuted, CanAddCommandExecuted );
+        }
+    }
+}
